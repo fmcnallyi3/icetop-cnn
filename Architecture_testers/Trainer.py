941029137b547e5d62cef37b9b62d1a4ad1bc73e
Peter Richardson <peterkrichardson1@gmail.com>
2022-02-12 02:40:44 -0500

    finished dataprep iterative trainer

--- Architecture_testers/Trainer.py
+++ Architecture_testers/Trainer.py
@@ -3,227 +3,248 @@ import numpy as np
 from tensorflow import keras
 from keras import layers, models, callbacks
 import pickle
-
+import os
 from multiprocessing import Process
 
-class Trainer:
+def generate_data_prep(q = None, t = None,  normed = None, reco = None, cosz = None):
+    #parameters specific combos
+    default_options = { 'q' :  ["mean","sum","product","min","max", None], 
+                        't' : ["mean","sum","product","min","max", None, False], #this may be wrong, check in datatool
+                        'normed' : [True, False],
+                        'reco' : [ None, "plane", "laputop", "small"],
+                        'cosz' : [False, True]}
 
-    def __init__(self, name = None, model = None , data_prep = None):
-        self.model = None 
-        self.data_preps = [] #list of all possible data preps based on user choice
-        #self.data_prep=None
-        self.name=None
+    if q!=None:
+        default_options["q"]=q
+    if t!=None:
+        default_options["t"]=t
+    if normed!=None:
+        default_options["normed"]=normed
+    if reco!=None:
+        default_options["reco"]=reco
+    if cosz!=None:
+        default_options["cosz"]=cosz
 
-        #self.test_specific = False #is true when the user wants to test a particular data_prep
+
+    data_preps = []
+
+    for charge in default_options["q"]:
+        for time in default_options["t"]:
+            for norm in default_options["normed"]:
+                for rec in default_options["reco"]:
+                    for cos in default_options["cosz"]:
+                        if ( (rec!='None' and cosz!=True) or ((charge is None) and (time is None) and (rec is None)) ): #impossible cases go here
+                            data_preps.append({"q": charge, "t": time, 
+                                                                    "normed": norm, "reco": rec,"cosz": cos })
+    return data_preps
+
+
+def compileModel(name, q=None, t=None, normed=False, reco=None, cosz=False):
     
-        if model!=None:
-            self.model=model
-        if data_prep!=None:
-            self.data_preps=[data_prep]
-        if name!=None:
-            self.name=name
-        elif(data_prep != None):
-            self.name=""
-            for _,value in data_prep.items():
-                if not value:
-                    self.name+=value
-                elif value is None:
-                    self.name+='None'
-                else:
-                    self.name+='False'
-            
-        
-        #load the simulation files
-        simPrefix = '/Users/kmays/simFiles'
+    ##4 layers | t=none & q = none
+    #if data_prep["t"] == None & data_prep["q"] == None:
+    #    dimensions=4
+    ##3 layers | t!=none & t!= false & q = none
+    #elif data_prep["t"] == False:
+    #    if data_prep["q"] == None:
+    #        dimensions=2
+    #    else:
+    #        dimensions=1
+    #else:
+    #    dimensions=3
+    ##3 layers | t=none & q != none
+    ##2 layers | t=false & q = none
+    ##1 layer | t=false & q != none
 
-        #load the data into their arrays
-        self.x, self.y = load_preprocessed(simPrefix, 'train')
-        self.x_i = None
-        self.temp_y = self.y['energy']
-        self.trainCut = (np.random.uniform(size=len(self.temp_y)) < 0.85)
-        self.testCut = np.logical_not(self.trainCut)
-        #changes later during training iteration
-
-
-    def main():
-        Trainer()
-
-
-    def generate_data_prep(self, q = None, t = None,  normed = None, reco = None, cosz = None):
-        #parameters specific combos
-        default_options = { 'q' :  ["mean","sum","product","min","max", None], 
-                            't' : ["mean","sum","product","min","max", None, False], #this may be wrong, check in datatool
-                            'normed' : [True, False],
-                            'reco' : [ None, "plane", "laputop", "small"],
-                            'cosz' : [False, True]}
     
-        if q!=None:
-            default_options["q"]=q
-        if t!=None:
-            default_options["t"]=t
-        if normed!=None:
-            default_options["normed"]=normed
-        if reco!=None:
-            default_options["reco"]=reco
-        if cosz!=None:
-            default_options["cosz"]=cosz
+    if t is None:
+        tdim = 2
+    elif not t:
+        tdim = 0
+    else:
+        tdim = 1
+
+    if q is None:
+        qdim = 2
+    elif q is False:
+        qdim = 0
+    else:
+        qdim = 1
 
 
-      
-        for charge in default_options["q"]:
-            for time in default_options["t"]:
-                for norm in default_options["normed"]:
-                    for rec in default_options["reco"]:
-                        for cos in default_options["cosz"]:
-                            if (rec!='None' and cosz!=True): #impossible cases go here
-                                self.data_preps.append({"q": charge, "t": time, 
-                                                                      "normed": norm, "reco": rec,"cosz": cos })
-    
+    #actual model layers
+    ##CHARGE
+    if qdim != 0:
+        charge_input = keras.Input(shape=(10,10,qdim,),name="charge")
+        #charge_input=keras.Input(shape=(10,10,2,))
+        convq1_layer = layers.Conv2D(64,kernel_size=3,padding='same',activation='relu')(charge_input)
+        convq2_layer = layers.Conv2D(32,kernel_size=3,padding='same',activation='relu')(convq1_layer)
+        convq3_layer = layers.Conv2D(16, kernel_size=3, padding='same',activation="relu")(convq2_layer)
+        flatq_layer = layers.Flatten()(convq3_layer)
 
-    def compileModel(self, data_prep):
-        #4 layers | t=none & q = none
-        if data_prep["t"] == None & data_prep["q"] == None:
-            dimensions=4
-        #3 layers | t!=none & t!= false & q = none
-        elif data_prep["t"] == False:
-            if data_prep["q"] == None:
-                dimensions=2
-            else:
-                dimensions=1
-        else:
-            dimensions=3
-        #3 layers | t=none & q != none
-        #2 layers | t=false & q = none
-        #1 layer | t=false & q != none
+    ##TIME
+    if tdim != 0:
+        time_input = keras.Input(shape=(10,10,tdim,),name="time")
+        #time_input=keras.Input(shape=(10,10,2,))
+        convt1_layer = layers.Conv2D(64,kernel_size=3,padding='same',activation='relu')(time_input)
+        convt2_layer = layers.Conv2D(32,kernel_size=3,padding='same',activation='relu')(convt1_layer)
+        convt3_layer = layers.Conv2D(16, kernel_size=3, padding='same',activation="relu")(convt2_layer)
+        flatt_layer = layers.Flatten()(convt3_layer)
 
-        #actual model layers
-        charge_input = keras.Input(shape=(10,10,dimensions,),name="charge")
-        charge_input=keras.Input(shape=(10,10,2,))
-
-        conv1_layer = layers.Conv2D(64,kernel_size=3,padding='same',activation='relu')(charge_input)
-        conv2_layer = layers.Conv2D(32,kernel_size=3,padding='same',activation='relu')(conv1_layer)
-        conv3_layer = layers.Conv2D(16, kernel_size=3, padding='same',activation="relu")(conv2_layer)
-        flat_layer = layers.Flatten()(conv3_layer)
+    if not (reco is None):
         zenith_input=keras.Input(shape=(1,))
-        concat_layer = layers.Concatenate()([flat_layer,zenith_input])
-        dense1_layer = layers.Dense(256,activation='relu')(concat_layer)
-        dense2_layer = layers.Dense(256,activation='relu')(dense1_layer)
-        dense3_layer = layers.Dense(256,activation="relu")(dense2_layer)
-        output = layers.Dense(1)(dense3_layer)
+        if tdim != 0:
+            if qdim != 0:
+                concat_layer = layers.Concatenate()([flatq_layer,flatt_layer,zenith_input])
+                dense1_layer = layers.Dense(256,activation='relu')(concat_layer)
+            else:
+                concat_layer = layers.Concatenate()([flatt_layer,zenith_input])
+                dense1_layer = layers.Dense(256,activation='relu')(concat_layer)
+        elif qdim != 0:
+            concat_layer = layers.Concatenate()([flatq_layer,zenith_input])
+            dense1_layer = layers.Dense(256,activation='relu')(concat_layer)
+        else:
+            dense1_layer = layers.Dense(256,activation='relu')(zenith_input)
+    else:
+        if tdim != 0:
+            if qdim != 0:
+                concat_layer = layers.Concatenate()([flatq_layer,flatt_layer])
+                dense1_layer = layers.Dense(256,activation='relu')(concat_layer)
+            else:
+                dense1_layer = layers.Dense(256,activation='relu')(flatt_layer)
+        elif qdim != 0:
+            dense1_layer = layers.Dense(256,activation='relu')(flatq_layer)
+        else:
+            print("All inputs removed!")
+            return
 
-        self.model = models.Model(inputs=[charge_input,zenith_input],outputs=output,name=self.name)
-        self.model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae','mse'])
+    dense2_layer = layers.Dense(256,activation='relu')(dense1_layer)
+    dense3_layer = layers.Dense(256,activation="relu")(dense2_layer)
+    output = layers.Dense(1)(dense3_layer)
 
-    #outside training loop:
-        #load data, name., data_prep combos, 
-    #every iteration:
-        #specific data prep, model, save function.
+    if not (reco is None):
+        if tdim != 0:
+            if qdim != 0:
+                model = models.Model(inputs=[charge_input,time_input,zenith_input],outputs=output,name=name)
+            else:
+                model = models.Model(inputs=[time_input,zenith_input],outputs=output,name=name)
+        elif qdim != 0:
+            model = models.Model(inputs=[charge_input,zenith_input],outputs=output,name=name)
+        else:
+            model = models.Model(inputs=[zenith_input],outputs=output,name=name)
+    else:
+        if tdim != 0:
+            if qdim != 0:
+                model = models.Model(inputs=[charge_input,time_input],outputs=output,name=name)
+            else:
+                model = models.Model(inputs=[time_input],outputs=output,name=name)
+        elif qdim != 0:
+            model = models.Model(inputs=[charge_input],outputs=output,name=name)
+        else:
+            print("All inputs removed! Also, first check didn't trigger!")
+            return
+
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae','mse'])
+    return model
+
+#outside training loop:
+    #load data, name., data_prep combos, 
+#every iteration:
+    #specific data prep, model, save function.
+
+def train(data_prep, x, y, numepochs=100):
+    x_i = dataPrep(x, y, **data_prep)
+
+    for key in data_prep:
+        if key == "reco":
+            if data_prep[key] != None:
+                l = len(x_i) - 1
+                nancut=(x_i[l]==x_i[l])
+                for i in range(0,l):
+                    x_i[i]=x_i[i][nancut]
+                y = y[nancut]
+
+    name=""
+    for _,value in data_prep.items():
+        name+=str(value)
+    model = compileModel(name, **data_prep)
+    print("Training %s..." % str(data_prep))
+    csv_logger = callbacks.CSVLogger('trainedModels/{}'.format(name))
+    early_stop = callbacks.EarlyStopping(patience=10, restore_best_weights=True) # default -> val_loss
+    checkpoint = callbacks.ModelCheckpoint('trainedModels/%s.h5' % name,save_best_only=True)
+    callbacklist = [early_stop, csv_logger,checkpoint]
+    history = model.fit(x=x_i, y=y, epochs=numepochs,validation_split=0.15,callbacks=callbacklist)
+    with open('trainedModels/%s.pickle', 'wb') as f:
+        pickle.dump(history.history, f)
+
+
+
+#name file, make a method for it later or just input one for now
+
+#train (public)
+    #employ early stopping 
+
+#save everything function (private)
+
+    """ def __save():
+#save csv, DO NOT SAVE DATA PREP DIRECTLY, reconstuct from index
+#records data prep index, lowest loss, and number of epochs
+#stored into a csv file at each iteration
+
+#create row for saved information
+best_loss=history.history(['loss'][-1])
+num_epoch=len(history.history(['loss']))
+#data_prep_index=data_preps...
+new_row=[best_loss, num_epoch]
+
+#make sure to append to file
+#save the dataprep index as its own column so the info isn't lost?
+with open('DataResults/results%.csv' %it, 'a') as f_object:
+    csv_writer=writer(f_object)
+    csv_writer.writerow(new_row)
+    f_object.close()
     
-    def train(self, i,numepochs=100):
-        #if self.data_prep !=None:
-        #    self.x_i = dataPrep(self.x, self.y, **self.data_prep)
-        #    self.compileModel(self.data_prep)
-        #    
-        #    csv_logger = callbacks.CSVLogger('trainedModels/{}'.format(self.name))
-        #    early_stop = callbacks.EarlyStopping(patience=10, restore_best_weights=True) # default -> val_loss
-        #    checkpoint = callbacks.ModelCheckpoint('trainedModels/%s.h5' % self.name,save_best_only=True)
-        #    self.callbacklist = [early_stop, csv_logger,checkpoint]
-        #    history = self.model.fit(x=self.x_i, y=self.temp_y, epochs=numepochs,validation_split=0.15,callbacks=self.callbacklist)
-        #    with open('trainedModels/%s.pickle', 'wb') as f:
-        #        pickle.dump(history.history, f)
-
-        #else:
-            #for data_prep in self.data_preps:
-                data_prep = self.data_preps[i]
-                self.x_i = dataPrep(self.x, self.y, **data_prep)
-                self.compileModel(data_prep)
-                
-                self.name=""
-                for _,value in data_prep.items():
-                    if not value:
-                        self.name+=value
-                    elif value is None:
-                        self.name+='None'
-                    else:
-                        self.name+='False'
-                print("Training %s..." % self.name)
-                print(str(data_prep))
-                csv_logger = callbacks.CSVLogger('trainedModels/{}'.format(self.name))
-                early_stop = callbacks.EarlyStopping(patience=10, restore_best_weights=True) # default -> val_loss
-                checkpoint = callbacks.ModelCheckpoint('trainedModels/%s.h5' % self.name,save_best_only=True)
-                self.callbacklist = [early_stop, csv_logger,checkpoint]
-                history = self.model.fit(x=self.x_i, y=self.temp_y, epochs=numepochs,validation_split=0.15,callbacks=self.callbacklist)
-                with open('trainedModels/%s.pickle', 'wb') as f:
-                    pickle.dump(history.history, f)
-
-
-
-    #name file, make a method for it later or just input one for now
     
-    #train (public)
-        #employ early stopping 
-    
-    #save everything function (private)
+#save model
 
-                """ def __save():
-            #save csv, DO NOT SAVE DATA PREP DIRECTLY, reconstuct from index
-            #records data prep index, lowest loss, and number of epochs
-            #stored into a csv file at each iteration
-            
-            #create row for saved information
-            best_loss=history.history(['loss'][-1])
-            num_epoch=len(history.history(['loss']))
-            #data_prep_index=data_preps...
-            new_row=[best_loss, num_epoch]
-            
-            #make sure to append to file
-            #save the dataprep index as its own column so the info isn't lost?
-            with open('DataResults/results%.csv' %it, 'a') as f_object:
-                csv_writer=writer(f_object)
-                csv_writer.writerow(new_row)
-                f_object.close()
-            
-            
-        #save model
+def fileName():
+    #call this at the start of training set to determine where to save the file
+    #naming convention
+    key='nameHere'
+    it=1 #should this be universal?
+    while(os.path.isfile('DataResults/results%.csv' %it)):
+            it+1
+    key+=it
         
-        def fileName():
-            #call this at the start of training set to determine where to save the file
-            #naming convention
-            key='nameHere'
-            it=1 #should this be universal?
-            while(os.path.isfile('DataResults/results%.csv' %it)):
-                  it+1
-            key+=it
-            
-            
-    
-    #reconstruct data_prep from index(private)
-        def __fetch_dataprep():
-              #retrieves the index from the given ...
-    #######################################################################
-    
-    #analysis methods
-    
-    #get_lowest_loss 
-         def get_lowest_loss(r=None):
-              #retrieves the information from a file
-              #converts information to a dataframe
-              #retrieves the lowest or sorts then returns a range of the lowest loss
-              #returns another dataframe
-              if r=None:
-                  return min(history.history(['loss']))
-                  #get the lowest l
-              elif r>0:
-                  return np.partition(history.history['loss'],r)
-                  #get a list of length r of the lowest loss values
-              else:
-                  return 'please enter a valid number'
-                  #ask the use to enter a valid number 
-                  
-    #get_num_epochs
-    
-    #get_lowest_loss (with respect to number of epochs) (i.e loss vs epoch)
-    
-    #plot specific 
-    
-     """
+        
+
+#reconstruct data_prep from index(private)
+    def __fetch_dataprep():
+            #retrieves the index from the given ...
+#######################################################################
+
+#analysis methods
+
+#get_lowest_loss 
+        def get_lowest_loss(r=None):
+            #retrieves the information from a file
+            #converts information to a dataframe
+            #retrieves the lowest or sorts then returns a range of the lowest loss
+            #returns another dataframe
+            if r=None:
+                return min(history.history(['loss']))
+                #get the lowest l
+            elif r>0:
+                return np.partition(history.history['loss'],r)
+                #get a list of length r of the lowest loss values
+            else:
+                return 'please enter a valid number'
+                #ask the use to enter a valid number 
+                
+#get_num_epochs
+
+#get_lowest_loss (with respect to number of epochs) (i.e loss vs epoch)
+
+#plot specific 
+
+    """
--- Architecture_testers/tester.py
+++ Architecture_testers/tester.py
@@ -3,23 +3,32 @@ import numpy as np
 from tensorflow import keras
 from keras import layers, models, callbacks
 import pickle
+import os
+import multiprocessing
+from time import sleep
+from Trainer import generate_data_prep,train
 
-from multiprocessing import Process
-import Trainer
-
-trainer = Trainer()
-trainer.generate_data_prep()
-if not trainer.data_preps:
+data_preps = generate_data_prep()
+if not data_preps:
     print("Could not generate data preps.")
     quit()
-for prep in trainer.data_preps:
-    print(str(prep))
+
+simPrefix = os.getcwd()+'/simFiles'
+x, y = load_preprocessed(simPrefix, 'train')
+energy = y["energy"]
+
 processes = []
-if __name__ == '__main__':
-    i = 0
-    for prep in trainer.data_preps:
-        proc = Process(target=trainer.train(i))
-        proc.start()
-        i += 1
-    for proc in processes:
-        proc.join()
+#i = 0
+#for prep in data_preps:
+for i in range(0,1):
+    #print("Starting process for %s" % str(prep))
+    proc = multiprocessing.Process(target=train,args=(data_preps[i],x,energy,))
+    proc.start()
+    print("Started process %i: %s" % (proc.pid,str(data_preps[i])) )
+    processes.append(proc)
+    sleep(1)
+    print("Started process %i: %s" % (proc.pid,str(data_preps[i])) )
+#    i += 1
+    
+    #for proc in processes:
+    #    proc.join()
