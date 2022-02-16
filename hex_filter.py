9709d44e6f5d47d20412735d6e09d838ec9f0e26
Peter Richardson <peterkrichardson1@gmail.com>
2022-02-16 03:10:43 -0500

    hex filter WIP

--- hex_filter.py
+++ hex_filter.py
@@ -7,6 +7,7 @@ from keras import layers
 from keras import models
 from keras import callbacks
 from keras import backend
+import functools
 
 def hex_filter(shape, dtype=None):
     zeros=np.zeros
@@ -24,3 +25,53 @@ def hex_filter(shape, dtype=None):
     ])
     assert f.shape == shape
     return backend.variable(f,dtype='float32')
+
+#class MaskedConv2D(tf.keras.layers.Layer):
+#    def __init__(self, *args, **kwargs):
+#        super(MaskedConv2D, self).__init__()
+#        self.conv2d = tf.keras.layers.Conv2D(*args, **kwargs)
+#        self.mask = np.array([
+#            [0,1,1],
+#            [1,1,1],
+#            [1,1,0]
+#        ])
+#        self.mask = tf.reshape(self.mask, self.mask.shape + (1,1))
+#        
+#    def build(self, input_shape):
+#        self.conv2d.build(input_shape[0])
+#        self._convolution_op = self.conv2d._convolution_op
+#        
+#    def masked_convolution_op(self, filters, kernel, mask):
+#        return self._convolution_op(filters, tf.math.multiply(kernel, self.mask))
+#        
+#    def call(self, inputs):
+#        x = inputs
+#        self.conv2d._convolution_op = functools.partial(self.masked_convolution_op, mask=self.mask)
+#        return self.conv2d.call(x)
+
+#class MaskedConv2D(layers.Conv2D):
+#    def call(self, inputs):
+#        mask=np.array([
+#            [0,1,1],
+#            [1,1,1],
+#            [1,1,0]])
+#        result = self.convolution_op(inputs, tf.math.multiply(kernel,mask))
+#        if self.use_bias:
+#            result = result + self.bias
+#        return result
+    
+class MaskedConv2D(layers.Conv2D):
+    def convolution_op(self, inputs, kernel):
+        mask=np.array([
+            [0,1,1],
+            [1,1,1],
+            [1,1,0]])
+        mask = tf.reshape(mask,mask.shape+(1,1))
+        print(kernel)
+        print(tf.math.multiply(kernel,mask))
+        return tf.nn.conv2d(
+            inputs,
+            tf.math.multiply(kernel,mask),
+            padding="SAME",
+            strides=[1,1]
+        )
