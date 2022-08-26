5368d12b3a4375d2fe7fce11a176b567644c7e28
EthanDorr <106080973+EthanDorr@users.noreply.github.com>
2022-08-26 17:12:07 -0400

    Add files via upload

--- data_tools.py
+++ data_tools.py
@@ -1,86 +1,86 @@
-import ast
-import numpy as np
-import sys
-import warnings
+from ast import literal_eval
 from collections import defaultdict
 from glob import glob
+from sys import version_info
+from warnings import catch_warnings, filterwarnings
+import numpy as np
 
 
 # Modified logarithm convenient for dealing with zeros and maintaining sign
-def modLog(n):
+def mod_log(n):
     return np.sign(n) * np.log10(np.abs(n) + 1)
 
 
 # Reverse the above process
-def rLog(m):
+def r_log(m):
     return np.sign(m) * (10**np.abs(m) - 1)
 
 
 # Hard-coded string to pix stolen from Mirco (deepLearning/hexagon/hexConv.py)
-def hexSquare():
-    sta2hex = {'01':(-4,-1), '02':(-4,0), '03':(-4,1), '04':(-4,2), '05':(-4,3), '06':(-4,4), '07':(-3,-2), '08':(-3,-1), '09':(-3,0), '10':(-3,1), '11':(-3,2), '12':(-3,3), '13':(-3,4), '14':(-2,-3), '15':(-2,-2), '16':(-2,-1), '17':(-2,0), '18':(-2,1), '19':(-2,2), '20':(-2,3), '21':(-2,4), '22':(-1,-4), '23':(-1,-3), '24':(-1,-2), '25':(-1,-1), '26':(-1,0), '27':(-1,1), '28':(-1,2), '29':(-1,3), '30':(-1,4), '31':(0,-5), '32':(0,-4), '33':(0,-3), '34':(0,-2), '35':(0,-1), '36':(0,0), '37':(0,1), '38':(0,2), '39':(0,3), '40':(0,4), '41':(1,-5), '42':(1,-4), '43':(1,-3), '44':(1,-2), '45':(1,-1), '46':(1,0), '47':(1,1), '48':(1,2), '49':(1,3), '50':(1,4), '51':(2,-5), '52':(2,-4), '53':(2,-3), '54':(2,-2), '55':(2,-1), '56':(2,0), '57':(2,1), '58':(2,2), '59':(2,3), '60':(3,-5), '61':(3,-4), '62':(3,-3), '63':(3,-2), '64':(3,-1), '65':(3,0), '66':(3,1), '67':(3,2), '68':(4,-5), '69':(4,-4), '70':(4,-3), '71':(4,-2), '72':(4,-1), '73':(4,0), '74':(4,1), '75':(5,-5), '76':(5,-4), '77':(5,-3), '78':(5,-2)}
+def hex_square():
+    sta_to_hex = {'01':(-4,-1), '02':(-4,0), '03':(-4,1), '04':(-4,2), '05':(-4,3), '06':(-4,4), '07':(-3,-2), '08':(-3,-1), '09':(-3,0), '10':(-3,1), '11':(-3,2), '12':(-3,3), '13':(-3,4), '14':(-2,-3), '15':(-2,-2), '16':(-2,-1), '17':(-2,0), '18':(-2,1), '19':(-2,2), '20':(-2,3), '21':(-2,4), '22':(-1,-4), '23':(-1,-3), '24':(-1,-2), '25':(-1,-1), '26':(-1,0), '27':(-1,1), '28':(-1,2), '29':(-1,3), '30':(-1,4), '31':(0,-5), '32':(0,-4), '33':(0,-3), '34':(0,-2), '35':(0,-1), '36':(0,0), '37':(0,1), '38':(0,2), '39':(0,3), '40':(0,4), '41':(1,-5), '42':(1,-4), '43':(1,-3), '44':(1,-2), '45':(1,-1), '46':(1,0), '47':(1,1), '48':(1,2), '49':(1,3), '50':(1,4), '51':(2,-5), '52':(2,-4), '53':(2,-3), '54':(2,-2), '55':(2,-1), '56':(2,0), '57':(2,1), '58':(2,2), '59':(2,3), '60':(3,-5), '61':(3,-4), '62':(3,-3), '63':(3,-2), '64':(3,-1), '65':(3,0), '66':(3,1), '67':(3,2), '68':(4,-5), '69':(4,-4), '70':(4,-3), '71':(4,-2), '72':(4,-1), '73':(4,0), '74':(4,1), '75':(5,-5), '76':(5,-4), '77':(5,-3), '78':(5,-2)}
     # Inverse of above
-    hex2sta = {v:k for k, v in sta2hex.items()}
+    hex_to_sta = {v:k for k, v in sta_to_hex.items()}
 
-    return sta2hex, hex2sta
+    return sta_to_hex, hex_to_sta
 
 
 # Hard-coded edge stations on pixelized array (see backend notebook for more)
-def edgeIdxs():
+def edge_idxs():
     idxs = [[0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [1, 3], [1, 9], [2, 2], [2, 9], [3, 1], [3, 9], [4, 0], [4, 9], [5, 0], [5, 9], [6, 0], [6, 8], [7, 0], [7, 7], [8, 0], [8, 4], [8, 5], [8, 6], [9, 0], [9, 1], [9, 2], [9, 3]]
     return np.asarray(idxs)
 
 
 # Get cut to display on plots
-def getCut(cut_str, y, p, recoE, key):
+def get_cut(cut_str, y, p, recoE, key):
 
     # No Cut
     if cut_str == 'No Cut':
-        return np.array([True for _ in recoE[key]]), y['energy'][getRecoFilter(p[key]['reco'], y) * y['passed_STA5']] if p[key]['sta5'] else y['energy'][getRecoFilter(p[key]['reco'], y)]
+        return np.array([True for _ in recoE[key]]), (y['energy'][get_reco_nan_filter(p[key]['reco'], y)], y['energy'][get_reco_nan_filter(p[key]['reco'], y) * y['passed_STA5']])[p[key]['sta5']]
 
     # Quality Cut
     if cut_str == 'Quality Cut':
-        return y['quality_cut'][getRecoFilter(p[key]['reco'], y) * y['passed_STA5']] if p[key]['sta5'] else y['quality_cut'][getRecoFilter(p[key]['reco'], y)], y['energy'][y['quality_cut']]
+        return (y['quality_cut'][get_reco_nan_filter(p[key]['reco'], y)], y['quality_cut'][get_reco_nan_filter(p[key]['reco'], y) * y['passed_STA5']])[p[key]['sta5']], y['energy'][y['quality_cut']]
 
     raise Exception('Bad cut!')
 
 
-def load_data(fileList, infill=False):
+def load_data(file_list, infill=False):
 
     d = defaultdict(list)
-    singleKeys = ['gain','position'] # Keys not tied to events
-    domKeys = ['charge_HLC', 'charge_SLC', 'time_HLC', 'time_SLC'] # Keys tied to DOMs
+    single_keys = ['gain','position'] # Keys not tied to events
+    dom_keys = ['charge_HLC', 'charge_SLC', 'time_HLC', 'time_SLC'] # Keys tied to DOMs
 
     # Concatenate all information tied to events
-    for _, f in enumerate(fileList):
-        if sys.version_info.major == 3:
+    for _, f in enumerate(file_list):
+        if version_info.major == 3:
             d_i = np.load(f, allow_pickle=True)
         else:
             d_i = np.load(f)
         d_i = d_i.item()
         for key in d_i.keys():
-            if key not in singleKeys:
+            if key not in single_keys:
                 d[key] += d_i[key]
 
     # Only copy in the detector information once
-    for key in singleKeys:
+    for key in single_keys:
         d[key] = d_i[key]
 
     # Remove infill array
     if not infill:
-        for key in domKeys:
-            d[key] = [{k:v for k, v in domSeries.items() if int(k[:2])<79}
-                     for domSeries in d[key]]
+        for key in dom_keys:
+            d[key] = [{k:v for k, v in dom_series.items() if int(k[:2])<79}
+                     for dom_series in d[key]]
 
     return d
 
 
 """ Convert dictionary format to multi-layered array for use with CNN """
-def dict2mat(d):
+def dict_to_mat(d):
 
-    sta2hex, _ = hexSquare()
-    stationList = sorted(sta2hex.keys())
-    squareY, squareX = np.transpose([sta2hex[s] for s in stationList])
+    sta_to_hex, _ = hex_square()
+    station_list = sorted(sta_to_hex.keys())
+    squareY, squareX = np.transpose([sta_to_hex[s] for s in station_list])
     tank1s = ['61','62']
     tank2s = ['63','64']
 
@@ -93,28 +93,28 @@ def dict2mat(d):
     # Layers #1 and 3: HLC charge at each tank
     for i, charges in enumerate(d['charge_HLC']):
         for dom, charge in charges.items():
-            y_i = sta2hex[dom[:2]][0] - squareY.min()
-            x_i = sta2hex[dom[:2]][1] - squareX.min()
+            y_i = sta_to_hex[dom[:2]][0] - squareY.min()
+            x_i = sta_to_hex[dom[:2]][1] - squareX.min()
             if dom[2:] in tank1s:
-                data[i][y_i][x_i][0] += modLog(charge)
+                data[i][y_i][x_i][0] += mod_log(charge)
             if dom[2:] in tank2s:
-                data[i][y_i][x_i][2] += modLog(charge)
+                data[i][y_i][x_i][2] += mod_log(charge)
 
     # Layers #2 and 4: SLC charge at each tank
     for i, charges in enumerate(d['charge_SLC']):
         for dom, charge in charges.items():
-            y_i = sta2hex[dom[:2]][0] - squareY.min()
-            x_i = sta2hex[dom[:2]][1] - squareX.min()
+            y_i = sta_to_hex[dom[:2]][0] - squareY.min()
+            x_i = sta_to_hex[dom[:2]][1] - squareX.min()
             if dom[2:] in tank1s:
-                data[i][y_i][x_i][1] += modLog(charge)
+                data[i][y_i][x_i][1] += mod_log(charge)
             if dom[2:] in tank2s:
-                data[i][y_i][x_i][3] += modLog(charge)
+                data[i][y_i][x_i][3] += mod_log(charge)
 
     # Layers #5 and 7: HLC time at each tank
     for i, times in enumerate(d['time_HLC']):
         for dom, time in times.items():
-            y_i = sta2hex[dom[:2]][0] - squareY.min()
-            x_i = sta2hex[dom[:2]][1] - squareX.min()
+            y_i = sta_to_hex[dom[:2]][0] - squareY.min()
+            x_i = sta_to_hex[dom[:2]][1] - squareX.min()
             if dom[2:] in tank1s:
                 data[i][y_i][x_i][4] += time
             if dom[2:] in tank2s:
@@ -123,8 +123,8 @@ def dict2mat(d):
     # Layers #6 and 8: SLC time at each tank
     for i, times in enumerate(d['time_SLC']):
         for dom, time in times.items():
-            y_i = sta2hex[dom[:2]][0] - squareY.min()
-            x_i = sta2hex[dom[:2]][1] - squareX.min()
+            y_i = sta_to_hex[dom[:2]][0] - squareY.min()
+            x_i = sta_to_hex[dom[:2]][1] - squareX.min()
             if dom[2:] in tank1s:
                 data[i][y_i][x_i][5] += time
             if dom[2:] in tank2s:
@@ -133,7 +133,7 @@ def dict2mat(d):
     return data
 
 
-def load_preprocessed(filePath, mode, comp=['p','f']):
+def load_preprocessed(file_path, mode, comp=['p','f']):
 
     # Make sure user selects a valid mode
     if mode not in ['assessment', 'train', None]:
@@ -141,28 +141,26 @@ def load_preprocessed(filePath, mode, comp=['p','f']):
         raise
     
     # Load valid x and y files
-    xfiles = sorted(glob('%s/x_*.npy' % filePath))
-    yfiles = sorted(glob('%s/y_*.npy' % filePath))
+    x_files = sorted(glob('%s/x_*.npy' % file_path))
+    y_files = sorted(glob('%s/y_*.npy' % file_path))
 
-    # Load only proton and iron files
-    compDict = {'p':'12360','h':'12630','o':'12631','f':'12362'}
-    simList = [compDict[c] for c in comp]
+    # Load only files specified in comp (default proton and iron)
+    comp_dict = {'p':'12360','h':'12630','o':'12631','f':'12362'}
+    sim_list = [comp_dict[c] for c in comp]
         
-    xfiles = [f for f in xfiles if any([sim in f for sim in simList])]
-    yfiles = [f for f in yfiles if any([sim in f for sim in simList])]
-    #xfiles = [f for f in xfiles if '12630' not in f and '12631' not in f]
-    #yfiles = [f for f in yfiles if '12630' not in f and '12631' not in f]
+    x_files = [f for f in x_files if any([sim in f for sim in sim_list])]
+    y_files = [f for f in y_files if any([sim in f for sim in sim_list])]
 
     x = []
     y = defaultdict(list)
-    for i, xf in enumerate(xfiles):
+    for i, xf in enumerate(x_files):
         x_i = np.load(xf)
         x += [r for r in x_i]
         # Loading code specific to python version
-        if sys.version_info.major == 3:
-            y_i = np.load(yfiles[i], allow_pickle=True, encoding='latin1')
+        if version_info.major == 3:
+            y_i = np.load(y_files[i], allow_pickle=True, encoding='latin1')
         else:
-            y_i = np.load(yfiles[i])
+            y_i = np.load(y_files[i])
         y_i = y_i.item()
         for key in y_i.keys():
             y[key] += [r for r in y_i[key]]
@@ -191,7 +189,7 @@ def load_preprocessed(filePath, mode, comp=['p','f']):
  - Can combine charge or time layers to one value per station
  - Can normalize charge, time, or both
 """
-def dataPrep(x, y, clc=True, sta5=False, q=None, t=None, t_shift=False, normed=False, reco=None, cosz=False):
+def data_prep(x, y, clc=True, sta5=False, q=None, t=None, t_shift=False, t_clip=0, normed=False, reco=None, cosz=False):
 
     if clc:
         q1h, q1s, q2h, q2s, t1h, t1s, t2h, t2s = x.transpose(3,0,1,2)
@@ -206,14 +204,14 @@ def dataPrep(x, y, clc=True, sta5=False, q=None, t=None, t_shift=False, normed=F
         for key in y.keys():
             y[key] = y[key][passed_STA5]
 
-    qtDict = {'q':q, 't':t}
+    qt_dict = {'q':q, 't':t}
     new_dim = 4
-    for k, mergeType in qtDict.items():
+    for k, merge_type in qt_dict.items():
         # False = remove the layer
-        if mergeType == False:
+        if merge_type == False:
             new_dim -= 2
         # None indicates do nothing to the layers, everything else merges
-        elif mergeType != None:
+        elif merge_type != None:
             new_dim -= 1
 
     out_shape = (x.shape[0], x.shape[1], x.shape[2], new_dim)
@@ -224,20 +222,20 @@ def dataPrep(x, y, clc=True, sta5=False, q=None, t=None, t_shift=False, normed=F
     ## - use time associated with maximum charge
 
     # Setup for merging tanks to stations in common ways
-    for k, mergeType in qtDict.items():
+    for k, merge_type in qt_dict.items():
 
-        if mergeType == False:
+        if merge_type == False:
             continue
 
         if k == 'q':
-            if mergeType == None:
+            if merge_type == None:
                 out_array[...,:2] = x[...,:2]
                 continue
             a1, a2 = x[...,:2].transpose(3,0,1,2)
             out_idx = 0
             
         if k == 't':
-            if mergeType == None:
+            if merge_type == None:
                 out_array[...,-2:] = x[...,-2:]
                 continue
             a1, a2 = x[...,-2:].transpose(3,0,1,2)
@@ -246,51 +244,58 @@ def dataPrep(x, y, clc=True, sta5=False, q=None, t=None, t_shift=False, normed=F
         # Need to convert zeros for mean/min/max functions
         # Note: if there are np.nan's in data and you run the mean function,
         # they will be converted to zeros upon exit!
-        ignoreValues = {'mean':np.nan, 'min':np.inf, 'max':-np.inf}
-        if mergeType in ignoreValues.keys():
-            a1[a1==0] = ignoreValues[mergeType]
-            a2[a2==0] = ignoreValues[mergeType]
+        ignore_values = {'mean':np.nan, 'min':np.inf, 'max':-np.inf}
+        if merge_type in ignore_values.keys():
+            a1[a1==0] = ignore_values[merge_type]
+            a2[a2==0] = ignore_values[merge_type]
 
         # Options for combining: mean, sum, product, min, max
-        if mergeType not in ['mean','sum','product','min','max']:
+        if merge_type not in ['mean','sum','product','min','max']:
             print('Unexpected argument for %s' % k)
             raise
 
         # Mean requires special conversion back (since np.nan!=np.nan)
-        if mergeType == 'mean':
-            with warnings.catch_warnings():
-                warnings.filterwarnings('ignore', category=RuntimeWarning, message='Mean of empty slice')
+        if merge_type == 'mean':
+            with catch_warnings():
+                filterwarnings('ignore', category=RuntimeWarning, message='Mean of empty slice')
                 new_out = np.nanmean([a1,a2], axis=0)
             a1[a1!=a1] = 0
             a2[a2!=a2] = 0
             new_out[new_out != new_out] = 0
-        if mergeType == 'sum':
-            new_out = modLog(rLog(a1) + rLog(a2))
-        if mergeType == 'product':
+        if merge_type == 'sum':
+            new_out = mod_log(r_log(a1) + r_log(a2))
+        if merge_type == 'product':
             new_out = a1 + a2   # sum of logs = product
-        if mergeType == 'min':
+        if merge_type == 'min':
             new_out = np.min([a1,a2], axis=0)
-        if mergeType == 'max':
+        if merge_type == 'max':
             new_out = np.max([a1,a2], axis=0)
 
         # Convert zeros back
-        if mergeType in ['min','max']:
-            a1[a1==ignoreValues[mergeType]] = 0
-            a2[a2==ignoreValues[mergeType]] = 0
-            new_out[new_out == ignoreValues[mergeType]] = 0
+        if merge_type in ['min','max']:
+            a1[a1==ignore_values[merge_type]] = 0
+            a2[a2==ignore_values[merge_type]] = 0
+            new_out[new_out == ignore_values[merge_type]] = 0
 
         out_array[...,out_idx] = new_out
 
     # Check if charge layers are merged, index is where charge layers end and time layers begin
     idx = 2 if q == None else 1
 
-    # Time Shift
-    if t_shift and t != False:
-        # Find nonzero minimum across all time layers
-        t_nonzero_min = np.min(out_array[...,idx:][np.nonzero(out_array[...,idx:])])
-        # Subtract nonzero mimimum from all nonzero values across all time layers
-        out_array[...,idx:][out_array[...,idx:] != 0] -= t_nonzero_min
-
+    if t != False:
+        # Prepare all nonzero time values and sort them in ascending order
+        nonzero_time_values = np.sort(x[...,idx:][np.nonzero(x[...,idx:])])
+        # Time Clip
+        if 0 <= t_clip <= 100:
+            # Find index of time value that corresponds to the % of data
+            clip_idx = int((len(nonzero_time_values) - 1) * (1 - t_clip*.01))
+            # Clip all time values that are greater than the time value corresponding to the % of data
+            out_array[...,idx:] = np.clip(out_array[...,idx:], None, nonzero_time_values[clip_idx])
+        # Time Shift
+        if t_shift:
+            # Subtract nonzero mimimum from all nonzero values across all time layers
+            out_array[...,idx:][np.nonzero(out_array[...,idx:])] -= nonzero_time_values[0]
+        
     # Normalization
     if normed:
         if q != False:
@@ -319,17 +324,17 @@ def dataPrep(x, y, clc=True, sta5=False, q=None, t=None, t_shift=False, normed=F
 
 
 """ Filter NaNs from reconstruction """
-def getRecoFilter(reco, y):
+def get_reco_nan_filter(reco, y):
     return [True for _ in y['energy']] if reco == None else ~np.isnan(y['{}_dir'.format(reco)].transpose()[0].astype('float'))
 
 
-def load_mc(filePath):
+def load_mc(file_path):
 
-    mcfiles = sorted(glob('%s/sim_*_mc.npy' % filePath))
+    mc_files = sorted(glob('%s/sim_*_mc.npy' % file_path))
 
     mc = defaultdict(list)
-    for _, f in enumerate(mcfiles):
-        if sys.version_info.major == 3:
+    for _, f in enumerate(mc_files):
+        if version_info.major == 3:
             mc_i = np.load(f, allow_pickle=True)
         else:
             mc_i = np.load(f)
@@ -343,18 +348,18 @@ def load_mc(filePath):
     return mc
 
 
-def nameModel(prep, prefix=''):
-    outstr = prefix
+def name_model(prep, prefix=''):
+    out_str = prefix
     for key, value in prep.items():
-        outstr = '{}_{}.{}'.format(outstr, key, value)
-    return outstr
+        out_str = '{}_{}.{}'.format(out_str, key, value)
+    return out_str
 
 
-def name2prep(name):
+def name_to_prep(name):
     prep = {}
     args = [a.split('.') for a in name.split('_') if len(a.split('.'))==2]
     for key, value in args:
-        try: prep[key] = ast.literal_eval(value)
+        try: prep[key] = literal_eval(value)
         except ValueError:
             prep[key] = value
     return prep
