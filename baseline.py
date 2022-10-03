52d35768171f76b441163d2ee01e90849d54344a
kennedym12 <k.mays0203@gmail.com>
2022-10-03 18:51:07 -0400

    trained a baseline model

--- baseline.py
+++ baseline.py
@@ -20,13 +20,13 @@ def main():
 
     # Edit these parameters
     # Too many to list, look in data_prep in data_tools for a better idea of what each does
-    prep = {'clc':True, 'sta5':False, 'q':None, 't':None, 't_shift':True, 't_clip':0, 'normed':True, 'reco':None, 'cosz':False, 'rot':True}
+    prep = {'clc':True, 'sta5':False, 'q':None, 't':None, 't_shift':True, 't_clip':0, 'normed':True, 'reco':None, 'cosz':False, 'rot':False}
 
     # Set the number of models to train under this CNN
     num_models_to_train = 1
 
     # Name for model(s)
-    model_name = 'rotations'
+    model_name = 'baseline'
 
     # Type of model to train
     model_type = 'bauwens'
