a129b11dd077e3e77b5c0887a64d500ca9339108
EthanDorr <106080973+EthanDorr@users.noreply.github.com>
2023-11-30 18:39:18 -0500

    Update sim-maker.py
    Added option to submit all composition types

--- data_extraction/sim-maker.py
+++ data_extraction/sim-maker.py
@@ -20,33 +20,34 @@ def main(args):
         os.mkdir(args.out_dir)
 
     l3sim = '/data/ana/CosmicRay/IceTop_level3/sim/IC86.2012'
-    comp = {'proton':12360, 'helium':12630, 'oxygen':12631, 'iron':12362}
-    ID = comp[args.composition]
+    comp = {'proton': [12360], 'helium': [12630], 'oxygen': [12631], 'iron': [12362], 'all': [12360, 12630, 12631, 12362]}
+    c = comp[args.composition]
 
-    gcd = f'{l3sim}/GCD/Level3_{ID}_GCD.i3.gz'
-    files = glob(f'{l3sim}/oldstructure/{ID}/Level3_IC86.2012_{ID}_Run??????.i3.gz')
-    if args.mc:
-        mc_prefix = '/data/sim/IceTop/2012/generated/CORSIKA-ice-top'
-        files = glob(f'{mc_prefix}/{ID}/topsimulator/*/*.i3.gz')
-    files.sort()
-
-    batch_list = [files[i:i+args.n] for i in range(0, len(files), args.n)]
-    for i, batch in enumerate(batch_list):
-        if args.test:
-            batch = batch[:5]
-        batched_files = ' '.join(batch)
-        out_file = f'{args.out_dir}/sim_{ID}_{i:03}.npy'
+    for ID in c:
+        gcd = f'{l3sim}/GCD/Level3_{ID}_GCD.i3.gz'
+        files = glob(f'{l3sim}/oldstructure/{ID}/Level3_IC86.2012_{ID}_Run??????.i3.gz')
         if args.mc:
-            exe = exe.replace('extractor', 'mc_extractor')
-            out_file = out_file.replace('.npy', '_mc.npy')
-        if args.test:
-            out_file = out_file.replace('.npy','_test.npy')
+            mc_prefix = '/data/sim/IceTop/2012/generated/CORSIKA-ice-top'
+            files = glob(f'{mc_prefix}/{ID}/topsimulator/*/*.i3.gz')
+        files.sort()
 
-        exelines = f'{exe} -g {gcd} -f {batched_files} -o {out_file}'
-        pysubmit(exelines, outdir=f'/home/{os.getlogin()}/npx4', test=args.test)
+        batch_list = [files[i:i+args.n] for i in range(0, len(files), args.n)]
+        for i, batch in enumerate(batch_list):
+            if args.test:
+                batch = batch[:5]
+            batched_files = ' '.join(batch)
+            out_file = f'{args.out_dir}/sim_{ID}_{i:03}.npy'
+            if args.mc:
+                exe = exe.replace('extractor', 'mc_extractor')
+                out_file = out_file.replace('.npy', '_mc.npy')
+            if args.test:
+                out_file = out_file.replace('.npy','_test.npy')
 
-        if args.test:
-            break
+            exelines = f'{exe} -g {gcd} -f {batched_files} -o {out_file}'
+            pysubmit(exelines, outdir=f'/home/{os.getlogin()}/npx4', test=args.test)
+
+            if args.test:
+                break
 
 
 if __name__ == "__main__":
@@ -54,7 +55,7 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser(
         description='Runs extractor.py on the cluster')
     p.add_argument('-c', '--composition', dest='composition', type=str,
-        default='proton', choices=['proton','helium','oxygen','iron'],
+        default='proton', choices=['proton','helium','oxygen','iron','all'],
         help='Composition you want to run over')
     p.add_argument('-n', '--n', dest='n', type=int,
         default=1000,
