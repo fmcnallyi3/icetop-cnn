bcb2bd855085885699537610deb4de9dc659d1ca
Johnathan Stowers <j.stowers1@icloud.com>
2026-09-16 07:40:14 -0500

    fix condor container universe submission

--- submit.py
+++ submit.py
@@ -69,7 +69,8 @@ def main(args):
         f'error = {os.path.join("condor", "errors", f"{args.model_name }.err")}',
         'notification = never',
         '',
-        f'+SingularityImage = "{os.path.join(os.sep, "data", "user", "fmcnally", "icetop-cnn", "tf.sif")}"',
+        'universe = container',
+        'container_image = /data/user/fmcnally/icetop-cnn/tf.sif',
         'should_transfer_files = YES',
         'when_to_transfer_output = ON_EXIT',
         f'initialdir = {ICETOP_CNN_DIR}',
@@ -77,7 +78,7 @@ def main(args):
         'request_memory = 16G',
         'request_gpus = 1',
         '',
-        'requirements = HasSingularity && CudaCapability',
+        'requirements = HasSingularity',
         '',
         'queue',
         ''
